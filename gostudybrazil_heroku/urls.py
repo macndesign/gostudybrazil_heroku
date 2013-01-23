dd83ab587d3a4a4a5482fa320afd0da15afe8d87
Mario Chaves <macndesign@gmail.com>
2013-01-23 11:18:34 -0300

    layouts e estaticos

--- gostudybrazil_heroku/urls.py
+++ gostudybrazil_heroku/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.conf.urls.defaults import patterns, include, url
+# static imports
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/admin/#hooking-adminsite-instances-into-your-urlconf
@@ -11,4 +15,39 @@ urlpatterns = patterns('',
     # Admin panel and documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+    
+    # Index
+    url(r'^$', 'django.views.generic.simple.direct_to_template',
+        {'template': 'index.html'}, name='index'),
+        
+    # About
+    url(r'^about/$', 'django.views.generic.simple.direct_to_template',
+        {'template': 'about.html'}, name='about'),
+    
+    # Courses
+    url(r'^courses/$', 'django.views.generic.simple.direct_to_template',
+        {'template': 'courses.html'}, name='courses'),
+    
+    # Blog
+    url(r'^blog/$', 'django.views.generic.simple.direct_to_template',
+        {'template': 'blog.html'}, name='blog'),
+    
+    # Contact
+    url(r'^contact/$', 'django.views.generic.simple.direct_to_template',
+        {'template': 'contact.html'}, name='contact'),
+    
+    # Programs
+    url(r'^programs/$', 'django.views.generic.simple.direct_to_template',
+        {'template': 'programs.html'}, name='programs'),
+    
+    # Housing
+    url(r'^housing/$', 'django.views.generic.simple.direct_to_template',
+        {'template': 'housing.html'}, name='housing'),
+    
+    # Partners
+    url(r'^partners/$', 'django.views.generic.simple.direct_to_template',
+        {'template': 'partners.html'}, name='partners'),
+
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
